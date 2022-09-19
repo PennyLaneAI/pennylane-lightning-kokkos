584135f587d16ef9236a473ccea526192c9516d0
Lee J. O'Riordan <loriordan@gmail.com>
2022-09-19 09:27:41 -0400

    Update windows tests

--- setup.py
+++ setup.py
@@ -111,7 +111,7 @@ if not os.getenv("READTHEDOCS"):
                 else:
                     configure_args += ["-DENABLE_OPENMP=OFF"]
             elif platform.system() == "Windows":
-                configure_args += []
+                configure_args += ["-T clangcl"] # only build with Clang under Windows
             else:
                 if platform.system() != "Linux":
                     raise RuntimeError(f"Unsupported '{platform.system()}' platform")
