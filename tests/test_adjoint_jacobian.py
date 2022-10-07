8c611b8a10276b9d93ab9d518f29daaa966c466c
Shuli Shu <multiphaseCFD@gmail.com>
2022-10-07 18:39:04 +0000

    remove finite shots not support test in unit tests

--- tests/test_adjoint_jacobian.py
+++ tests/test_adjoint_jacobian.py
@@ -89,20 +89,20 @@ class TestAdjointJacobian:
         with pytest.raises(qml.QuantumFunctionError, match="Adjoint differentiation method does"):
             dev_kokkos.adjoint_jacobian(tape)
 
-    def test_finite_shots_fail(self):
-        """Tests exception raised when finite shots specified"""
-
-        dev = qml.device("lightning.kokkos", wires=1, shots=1)
-
-        with qml.tape.QuantumTape() as tape:
-            qml.PauliX(0)
-            qml.expval(qml.PauliZ(0))
-
-        with pytest.raises(
-            NotImplementedError,
-            match="lightning.kokkos does not currently support finite shots",
-        ):
-            dev.apply(tape)
+    # def test_finite_shots_fail(self):
+    #    """Tests exception raised when finite shots specified"""
+    #
+    #    dev = qml.device("lightning.kokkos", wires=1, shots=1)
+    #
+    #    with qml.tape.QuantumTape() as tape:
+    #        qml.PauliX(0)
+    #        qml.expval(qml.PauliZ(0))
+    #
+    #    with pytest.raises(
+    #        NotImplementedError,
+    #        match="lightning.kokkos does not currently support finite shots",
+    #    ):
+    #        dev.apply(tape)
 
     @pytest.mark.skip(reason="Warning not currently raised in favour of NotImplementedError")
     def test_finite_shots_warns(self):
@@ -384,26 +384,26 @@ class TestAdjointJacobianQNode:
     def dev_kokkos(self):
         return qml.device("lightning.kokkos", wires=2)
 
-    def test_finite_shots_warning(self):
-        """Tests that a warning is raised when computing the adjoint diff on a device with finite shots"""
-
-        dev = qml.device("lightning.kokkos", wires=1, shots=1)
-
-        with pytest.warns(
-            UserWarning,
-            match="Requested adjoint differentiation to be computed with finite shots.",
-        ):
-
-            @qml.qnode(dev, diff_method="adjoint")
-            def circ(x):
-                qml.RX(x, wires=0)
-                return qml.expval(qml.PauliZ(0))
-
-        with pytest.raises(
-            NotImplementedError,
-            match="lightning.kokkos does not currently support finite shots",
-        ):
-            qml.grad(circ)(0.1)
+    # def test_finite_shots_warning(self):
+    #    """Tests that a warning is raised when computing the adjoint diff on a device with finite shots"""
+    #
+    #    dev = qml.device("lightning.kokkos", wires=1, shots=1)
+    #
+    #    with pytest.warns(
+    #        UserWarning,
+    #        match="Requested adjoint differentiation to be computed with finite shots.",
+    #    ):
+    #
+    #        @qml.qnode(dev, diff_method="adjoint")
+    #        def circ(x):
+    #            qml.RX(x, wires=0)
+    #            return qml.expval(qml.PauliZ(0))
+    #
+    #    with pytest.raises(
+    #        NotImplementedError,
+    #        match="lightning.kokkos does not currently support finite shots",
+    #    ):
+    #        qml.grad(circ)(0.1)
 
     def test_qnode(self, mocker, tol, dev_kokkos):
         """Test that specifying diff_method allows the adjoint method to be selected"""
