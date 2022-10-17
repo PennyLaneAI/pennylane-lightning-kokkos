28e9152a0d3aaa404144cd968428a5afa910f120
Shuli Shu <multiphaseCFD@gmail.com>
2022-10-17 23:42:20 +0000

    add functor

---
+++ pennylane_lightning_kokkos/src/simulator/MeasuresFunctors.hpp
@@ -0,0 +1,83 @@
+#pragma once
+
+#include <Kokkos_Core.hpp>
+#include <Kokkos_StdAlgorithms.hpp>
+
+#include "Util.hpp"
+
+namespace {
+using namespace Pennylane::Util;
+namespace KE = Kokkos::Experimental;
+} // namespace
+
+namespace Pennylane {
+namespace Functors {
+/**
+ * @brief Compute sub-probability distribution from StateVector.
+ *
+ * @param arr_ StateVector data.
+ * @param probabilities_ Sub discrete probability distribution.
+ */
+template <class Precision> struct getSubProbFunctor {
+
+    Kokkos::View<Kokkos::complex<Precision> *> arr;
+    Kokkos::View<Precision *> probability;
+    Kokkos::View<size_t *> all_indices;
+    Kokkos::View<size_t *> all_offsets;
+
+    getSubProbFunctor(Kokkos::View<Kokkos::complex<Precision> *> arr_,
+                      Kokkos::View<Precision *> probability_,
+                      Kokkos::View<size_t *> all_indices_,
+                      Kokkos::View<size_t *> all_offsets_)
+        : arr(arr_), probability(probability_), all_indices(all_indices_),
+          all_offsets(all_offsets_) {}
+
+    KOKKOS_INLINE_FUNCTION
+    void operator()(const size_t i, const size_t j) const {
+        size_t index = all_indices[i] + all_offsets[j];
+        Precision REAL = arr[index].real();
+        Precision IMAG = arr[index].imag();
+        Precision value = REAL * REAL + IMAG * IMAG;
+        Kokkos::atomic_add(&probability[i], value);
+    }
+};
+
+template <class Precision> struct getTransposedIndexFunctor {
+
+    Kokkos::View<size_t *> sorted_ind_wires;
+    Kokkos::View<size_t *> trans_index;
+    getTransposedIndexFunctor(Kokkos::View<size_t *> sorted_ind_wires_,
+                              Kokkos::View<size_t *> trans_index_)
+        : sorted_ind_wires(sorted_ind_wires_), trans_index(trans_index_) {}
+
+    KOKKOS_INLINE_FUNCTION
+    void operator()(const size_t i, const size_t j) const {
+
+        size_t axis = sorted_ind_wires[j];
+        size_t index = i / (1L << j);
+
+        Kokkos::atomic_add(&trans_index[i], (index % 2) << axis);
+    }
+};
+
+template <class Precision> struct getTransposedFunctor {
+
+    Kokkos::View<Precision *> transProb;
+    Kokkos::View<Precision *> probability;
+    Kokkos::View<size_t *> trans_index;
+    getTransposedFunctor(Kokkos::View<Precision *> transProb_,
+                         Kokkos::View<Precision *> probability_,
+                         Kokkos::View<size_t *> trans_index_)
+        : transProb(transProb_), probability(probability_),
+          trans_index(trans_index_) {}
+
+    KOKKOS_INLINE_FUNCTION
+    void operator()(const size_t i) const {
+
+        size_t new_index = trans_index[i];
+        transProb[new_index] = probability[i];
+    }
+};
+
+} // namespace Functors
+} // namespace Pennylane
