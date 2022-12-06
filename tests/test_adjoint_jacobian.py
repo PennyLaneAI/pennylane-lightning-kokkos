8b563b4cb17417d291be5f2e002359a5a4b18807
Shuli Shu <08cnbj@gmail.com>
2022-12-06 15:17:04 -0500

    update wording

--- tests/test_adjoint_jacobian.py
+++ tests/test_adjoint_jacobian.py
@@ -12,7 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 """
-Tests for the ``adjoint_jacobian`` method of LightningKOKKOS.
+Tests for the ``adjoint_jacobian`` method of LightningKokkos.
 """
 import math
 import pytest
