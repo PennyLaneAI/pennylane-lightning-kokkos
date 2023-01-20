212ba1fe7b642d1187fd97326b56873e6e7dfd2f
Shuli Shu <multiphaseCFD@gmail.com>
2023-01-20 23:04:43 +0000

    quick fix

--- tests/test_adjoint_jacobian.py
+++ tests/test_adjoint_jacobian.py
@@ -288,7 +288,6 @@ class TestAdjointJacobian:
 
             qml.Rot(1.3, -2.3, 0.5, wires=[0])
             qml.RZ(-0.5, wires=0)
-            # qml.RY(0.5, wires=1).inv()
             qml.CNOT(wires=[0, 1])
 
             qml.expval(obs(wires=0))
@@ -627,13 +626,10 @@ def circuit_ansatz(params, wires):
     qml.QubitStateVector(unitary_group.rvs(2**4, random_state=0)[0], wires=wires)
     qml.RX(params[0], wires=wires[0])
     qml.RY(params[1], wires=wires[1])
-    # qml.RX(params[2], wires=wires[2]).inv()
     qml.RZ(params[0], wires=wires[3])
     qml.CRX(params[3], wires=[wires[3], wires[0]])
     qml.PhaseShift(params[4], wires=wires[2])
     qml.CRY(params[5], wires=[wires[2], wires[1]])
-    qml.CRZ(params[5], wires=[wires[0], wires[3]]).inv()
-    # qml.PhaseShift(params[6], wires=wires[0]).inv()
     qml.Rot(params[6], params[7], params[8], wires=wires[0])
     qml.MultiRZ(params[11], wires=[wires[0], wires[1]])
     qml.CPhase(params[12], wires=[wires[3], wires[2]])
@@ -647,7 +643,6 @@ def circuit_ansatz(params, wires):
     qml.DoubleExcitationMinus(params[27], wires=[wires[2], wires[0], wires[1], wires[3]])
     qml.RX(params[28], wires=wires[0])
     qml.RX(params[29], wires=wires[1])
-    qml.Rot(params[8], params[8], params[9], wires=wires[1]).inv()
     qml.DoubleExcitationPlus(params[27], wires=[wires[2], wires[0], wires[1], wires[3]])
 
 
