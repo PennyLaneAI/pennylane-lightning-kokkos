fb97b65af6e03f32e6cae212bbf6753379205ecf
Shuli Shu <08cnbj@gmail.com>
2023-02-14 13:41:54 -0500

    make format

--- pennylane_lightning_kokkos/lightning_kokkos.py
+++ pennylane_lightning_kokkos/lightning_kokkos.py
@@ -291,9 +291,7 @@ class LightningKokkos(LightningQubit):
         if observable.name != "PauliZ":
             self.apply_cq(observable.diagonalizing_gates())
             self._samples = self.generate_samples()
-        return super().sample(
-            observable, shot_range=shot_range, bin_size=bin_size, counts=counts
-        )
+        return super().sample(observable, shot_range=shot_range, bin_size=bin_size, counts=counts)
 
     def expval(self, observable, shot_range=None, bin_size=None):
         if observable.name in [
